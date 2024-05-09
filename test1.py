80a8b2c588c7a9c760d9b0933dabcbc7c75ad36f
Goncayuksekkaya <164162856+Goncayuksekkaya@users.noreply.github.com>
2024-05-09 20:48:29 +0300

    update test1.py

--- test1.py
+++ test1.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
 import unittest
 
 ####-Kullanıcı adı ve şifre alanları boş geçildiğinde uyarı mesajı olarak "Epic sadface: Username is required" gösterilmelidir.
@@ -8,7 +9,7 @@ import unittest
  
 class Test_class(unittest.TestCase): # yeni bir class oluşturduk
 
-    def gonca_test1(self):  # yeni bir method oluşturduk
+    def test_case1(self):  # yeni bir method oluşturduk
 
         driver = webdriver.Chrome() # chrome browser açmak için driver değişkenine eşitledik
 
@@ -33,7 +34,7 @@ class Test_class(unittest.TestCase): # yeni bir class oluşturduk
 
 ###-Sadece şifre alanı boş geçildiğinde uyarı mesajı olarak "Epic sadface: Password is required" gösterilmelidir.
 
-    def gonca_test2(self):  # yeni bir method oluşturduk
+    def test_case2(self):  # yeni bir method oluşturduk
 
         driver = webdriver.Chrome() # chrome browser açmak için driver değişkenine eşitledik
 
@@ -63,7 +64,7 @@ class Test_class(unittest.TestCase): # yeni bir class oluşturduk
         print("2. test başarılı!")
 
 ###-Kullanıcı adı "locked_out_user" şifre alanı "secret_sauce" gönderildiğinde "Epic sadface: Sorry, this user has been locked out." mesajı gösterilmelidir.
-    def gonca_test3(self):
+    def test_case3(self):
         driver=webdriver.Chrome()
         driver.get("https://www.saucedemo.com/")
         driver.maximize_window()
@@ -81,8 +82,8 @@ class Test_class(unittest.TestCase): # yeni bir class oluşturduk
         self.assertEqual(error_message,expected_message,"Beklenen mesajla ekrandaki mesaj uyumlu değil!") # ekrandaki mesaj ile beklediğimiz mesajı karşılaştırdık. Eğer aynı değilse konsola Beklenen mesajla ekrandaki mesaj uyumlu değil! mesajını yazdırdık.
         print("3. test başarılı!")
 
-        ##-Kullanıcı adı "standard_user" şifre "secret_sauce" gönderildiğinde kullanıcı "/inventory.html" sayfasına gönderilmelidir. Giriş yapıldıktan sonra kullanıcıya gösterilen ürün sayısı "6" adet olmalıdır.
-    def gonca_test4(self):
+##-Kullanıcı adı "standard_user" şifre "secret_sauce" gönderildiğinde kullanıcı "/inventory.html" sayfasına gönderilmelidir. Giriş yapıldıktan sonra kullanıcıya gösterilen ürün sayısı "6" adet olmalıdır.
+    def test_case4(self):
         driver=webdriver.Chrome()
         driver.get("https://www.saucedemo.com/")
         driver.maximize_window()
@@ -94,34 +95,45 @@ class Test_class(unittest.TestCase): # yeni bir class oluşturduk
         sleep(1)
         loginButton = driver.find_element(By.ID,"login-button")
         loginButton.click()
-        sleep(10)
+        self.wait.until(expected_conditions.url_to_be("https://www.saucedemo.com/inventory.html"))
+        check_item = driver.find_elements(By.CLASS_NAME,"inventory_item")
+        self.assertEqual(6,len(check_item))
+        print("4. test başarılı!")
 
+## kullanıcı başarılı bir şekilde giriş yapıp daha sonra başarılı bir şekilde çıkış yapabilmelidir.
+    def test_case5(self):
+        driver=webdriver.Chrome()
+        driver.get("https://www.saucedemo.com/")
+        driver.maximize_window()
+        sleep(1)
+        fill_username = driver.find_element(By.ID,"user-name")
+        fill_username.send_keys("standard_user")
+        fill_password = driver.find_element(By.ID,"password")
+        fill_password.send_keys("secret_sauce")
+        sleep(1)
+        login_button = driver.find_element(By.ID,"login-button")
+        login_button.click()
+        sleep(1)
+        hamburger_menu = driver.find_element(By.ID,"react-burger-menu-btn")
+        hamburger_menu.click()
+        sleep(1)
+        logout_button = driver.find_element(By.ID,"logout_sidebar_link")
+        logout_button.click()
+        sleep(1)
+        login_button_value = driver.find_element(By.ID,"login-button").get_attribute("value")
+        expected_value = "Login"
+        self.assertEqual(expected_value,login_button_value,"Beklenen value değeri ile ekrandaki value değeri uyumsuz çıkış yapma başarısız!")
+        print("5. test başarılı!")
     
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 testClass = Test_class() # yazdığımız test class'ını çağırdık
-#testClass.gonca_test1() # yazdığımız test fonksiyonunu çalıştırmak için burada bu satırı yazıyoruz
-#testClass.gonca_test2() # yazdığımız test fonksiyonunu çalıştırmak için burada bu satırı yazıyoruz
-#testClass.gonca_test3()
-testClass.gonca_test4()
+# yazdığımız test fonksiyonunu çalıştırmak için burada bu satırı yazıyoruz
+testClass.test_case1() 
+testClass.test_case2() 
+testClass.test_case3()
+testClass.test_case4()
+testClass.test_case5()
 
         
 
